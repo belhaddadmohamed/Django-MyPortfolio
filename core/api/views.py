849c70c9977b6fb51af9fb470b354fcc16653424
belhaddadmohamed <belhaddad.mohamed7@gmail.com>
2025-12-06 18:47:57 +0100

    update list of objects

--- core/api/views.py
+++ core/api/views.py
@@ -7,6 +7,7 @@ class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
+    # Allow any user to create a contact message, but restrict other actions to admin users
     def get_permissions(self):
         if self.action in ['create']:
             self.permission_classes = [AllowAny]
@@ -23,8 +24,14 @@ class ProjectViewSet(viewsets.ModelViewSet):
         if self.action in ['list', 'retrieve']:
             self.permission_classes = [AllowAny]
         else:
-            self.permission_classes = [IsAdminUser]  # ['create', 'update', 'partial_update', 'destroy']
+            self.permission_classes = [IsAdminUser]
         return super().get_permissions()
+
+    # Avoid exposing unpublished projects to non-admin users
+    def get_queryset(self):
+        if self.request.user and self.request.user.is_staff:
+            return Project.objects.all()
+        return Project.objects.filter(published=True)
 
 
 class SkillViewSet(viewsets.ModelViewSet):
@@ -38,6 +45,12 @@ class SkillViewSet(viewsets.ModelViewSet):
             self.permission_classes = [IsAdminUser]  # ['create', 'update', 'partial_update', 'destroy']
         return super().get_permissions()
     
+    # Avoid exposing unpublished skills to non-admin users
+    def get_queryset(self):
+        if self.request.user and self.request.user.is_staff:
+            return Skill.objects.all()
+        return Skill.objects.filter(published=True)
+    
 
 class CertificateViewSet(viewsets.ModelViewSet):
     queryset = Certificate.objects.all()
@@ -49,6 +62,13 @@ class CertificateViewSet(viewsets.ModelViewSet):
         else:
             self.permission_classes = [IsAdminUser]  # ['create', 'update', 'partial_update', 'destroy']
         return super().get_permissions()
+
+    # Avoid exposing unpublished certificates to non-amdin users
+    def get_queryset(self):
+        if self.request.user and self.request.user.is_staff:
+            return Certificate.objects.all()
+        return Certificate.objects.filter(published=True)    
+    
     
 
 class NewsletterViewSet(viewsets.ModelViewSet):
